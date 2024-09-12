2daaae80aa8171ef65b785b18e0c25f8839db5f6
danielbragadasilva <danyelsgs@gmail.com>
2024-09-12 18:44:52 -0300

    menu aplication

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const start = () => {
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
     while(true){
-        let opcao = "sair"
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar Meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar Metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value:"sair"
+                },
+            ]
+        })
+
         switch(opcao) {
 
             case "cadastrar":
                 console.log("vamos cadastrar")
                 break
-            case "lista":
+            case "listar":
                 console.log("vamos listar")
                 break
             case "sair":
